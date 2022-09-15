d6b48cbd3f5d7254b52b8129cf748022ba67c2c0
Babafemi Jimoh-Kuku <babafemijk@gmail.com>
2022-09-14 17:44:07 -0700

    Added task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - print alphabet [a..z]
+* in lowercase
+* return: always void
+*/
+/* the function is of type void */
+void print_alphabet_x10(void)
+{
+	char i;
+	int j;
+
+	j = 0;
+	while (j < 10)
+	{
+		i = 'a';
+		while (i <= 'z')
+		{
+			_putchar(i);
+			i++;
+		}
+		_putchar('\n');
+		j++;
+	}
+}
