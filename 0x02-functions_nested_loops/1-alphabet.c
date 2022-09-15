3b779242305ac803677e98982c0edf9228c04b9b
Babafemi Jimoh-Kuku <babafemijk@gmail.com>
2022-09-14 17:41:56 -0700

    Added task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_alphabet - void
+* Description - print alphabet in lowercase
+* using _putchar with function void print_alphabet
+* It prints the alphabet in lowercase fallowed by a new line
+* Return: 0
+*/
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
